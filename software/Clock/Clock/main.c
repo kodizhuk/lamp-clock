e2e9fd15d6c406079b15e30ae8112c55ad3b348c
Petro <petro.zhuk.95@gmail.com>
2018-09-23 21:27:41 +0300

    add empty project

---
+++ software/Clock/Clock/main.c
@@ -0,0 +1,18 @@
+/*
+ * Clock.c
+ *
+ * Created: 18.09.2018 5:40:01:PM
+ * Author : Petro
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
