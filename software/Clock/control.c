103a96198f3df50f6f4f32c738ac8b62d79378c4
Petro <petro.zhuk.95@gmail.com>
2018-09-23 22:17:08 +0300

    fix bug - another library before control.h

--- software/Clock/control.c
+++ software/Clock/control.c
@@ -1,13 +1,14 @@
+#include "control.h"
 #include <avr/io.h>
 #include <avr/sfr_defs.h>
-#include "control.h "
+
 
 #define BTN0 PD0
 #define BTN2 PD1
 #define BTN1 PD2
 
 control_result result = NO_PRESS;
-	
+
 uint8_t permissNextClick[3] = {1,1,1};
 
 
@@ -28,61 +29,37 @@ control_result ControlCheck()
 			result = PRESS_LEFT;
 			permissNextClick[0] = 0;
 		}else
-			result = PRESSED_LEFT;
+		result = PRESSED_LEFT;
 	}else
 	{
 		permissNextClick[0] = 1;
 	}
 	//second
- 	if(bit_is_clear(PIND, BTN1))
- 	{
- 		if(permissNextClick[1])
- 		{
- 			result = PRESS_CENTER;
- 			permissNextClick[1] = 0;
- 		}else
-			result = PRESSED_CENTER;
- 	}else
- 	{
- 		permissNextClick[1] = 1;
- 	}
- 	//third
- 	if(bit_is_clear(PIND, BTN2))
- 	{
- 		if(permissNextClick[2])
- 		{
- 			result = PRESS_RIGHT;
- 			permissNextClick[2] = 0;
- 		}else
-			result = PRESSED_RIGHT;
- 	}else
- 	{
- 		permissNextClick[2] = 1;
- 	}
-		
+	if(bit_is_clear(PIND, BTN1))
+	{
+		if(permissNextClick[1])
+		{
+			result = PRESS_CENTER;
+			permissNextClick[1] = 0;
+		}else
+		result = PRESSED_CENTER;
+	}else
+	{
+		permissNextClick[1] = 1;
+	}
+	//third
+	if(bit_is_clear(PIND, BTN2))
+	{
+		if(permissNextClick[2])
+		{
+			result = PRESS_RIGHT;
+			permissNextClick[2] = 0;
+		}else
+		result = PRESSED_RIGHT;
+	}else
+	{
+		permissNextClick[2] = 1;
+	}
+	
 	return result;
 }
-
-//control_result EncoderBtnCheck()
-//{
-	//static uint8_t permissNextClick = 1;
-//
-	//btnClick = NO_PRESS;
-	//if(bit_is_clear(PIND,BTN))
-	//{
-		//if(permissNextClick)
-		//{
-			//btnClick = PRESS_CENTER;
-			//permissNextClick = 0;
-		//}
-		//else
-			//btnClick = NO_PRESS;
-	//}
-	//else 
-	//{
-		//btnClick = NO_PRESS;
-		//permissNextClick = 1;
-	//}
-	//
-	//return btnClick;
-//}
--- software/Clock/control.h
+++ software/Clock/control.h
@@ -1,8 +1,16 @@
+/*
+ * control.h
+ *
+ * Created: 23.09.2018 10:08:08:PM
+ *  Author: Petro
+ */ 
+
+
 #ifndef CONTROL_H_
 #define CONTROL_H_
 
-typedef enum {NO_PRESS, PRESS_LEFT, PRESS_CENTER, PRESS_RIGHT, 
-			PRESSED_LEFT, PRESSED_CENTER, PRESSED_RIGHT}control_result;
+typedef enum {NO_PRESS, PRESS_LEFT, PRESS_CENTER, PRESS_RIGHT,
+PRESSED_LEFT, PRESSED_CENTER, PRESSED_RIGHT}control_result;
 
 void ControlInit();
 control_result ControlCheck();
